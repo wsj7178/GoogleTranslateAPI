b809aa7eabd1e8799339d716adad84cf1171e514
JBTS-01-08 <JBTS-01-08@JBTS-01-08-PC>
2017-06-08 21:21:11 +0900

    almost

--- src/maru/network/WebRequest.java
+++ src/maru/network/WebRequest.java
@@ -1,16 +1,54 @@
 package maru.network;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class WebRequest {
 	public static final int GET = 0;
 	public static final int POST = 1;
 	
-	private String url;
+	private URL url;
 	
-	public WebRequest(String url) {
-		this.url = url;
+	public WebRequest(String url) throws MalformedURLException {
+		this.url = new URL(url);
 	}
 	
-	public String request(int type, String data) {
-		return null;
+	public String request(int type, String data) throws IOException {
+		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod((type == GET) ? "GET" : "POST");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(data);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + data);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		return response.toString();
 	}
 }
--- src/maru/translate/Translator.java
+++ src/maru/translate/Translator.java
@@ -1,7 +1,33 @@
 package maru.translate;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import maru.network.WebRequest;
+
 public class Translator {
 	public static String translate(String src) {
-		return null;
+		String result = null;
+		try {
+			WebRequest request = new WebRequest("https://translate.google.com/translate_a/single");
+			result = request.request(WebRequest.GET,"client=t&sl=ko&tl=en&hl=ko&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&source=btn&ssel=0&tsel=0&kc=0&tk=838310.692962&q=" + toHex(src));			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	private static String toHex(String src) {
+		StringBuffer buf = new StringBuffer();
+		for (char ch : src.toCharArray()) {
+			buf.append(String.format("%%%x", (int)ch));
+		}
+		return buf.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Translator.translate("안녕하세요"));
 	}
 }
